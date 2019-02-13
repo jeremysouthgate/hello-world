0d4b164b3fc9d4b09710424802321d15419d3818
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-12 21:48:27 -0500

    add index.php and /images

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset="utf-8"/ />
+</head>
+<body>
+    <h1>Hello World</h1>
+    <img src='./images/ice_cream.jpg' alt='Ice Cream'/>
+</body>
+</html>
